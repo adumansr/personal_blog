2187bc27c06be29c7e77f4ef17290666a9af4082
adumansr <muhammadumar1220@gmail.com>
2023-07-01 11:10:43 +0100

    edited the template to my prefrences

---
+++ src/content/post/newpost.md
@@ -0,0 +1,8 @@
+---
+title: "New post it currently is not up"
+description: "This post is purely for testing if the css is correct for the title on the page"
+publishDate: "01 Feb 2023"
+tags: ["test"]
+---
+
+## This post was made with aduman in mind
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,10 +4,6 @@ export const MENU_LINKS = [
 		path: "/",
 	},
 	{
-		title: "About",
-		path: "/about/",
-	},
-	{
 		title: "Blog",
 		path: "/posts/",
 	},
@@ -15,8 +11,8 @@ export const MENU_LINKS = [
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/chrismwilliams/astro-cactus",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
+	github: "https://github.com/adumansr",
+	twitter: "https://twitter.com/adumansr",
 	discord: "https://github.com/chrismwilliams/astro-cactus",
-	email: "me@example.com",
+	email: "muhammadumar1220@gmail.com",
 };
